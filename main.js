68b0de20286fae3904896817c34a77dd2c84d6ea
Browny <kyul4852@gmail.com>
2021-06-05 21:00:28 +0800

    add scroll effect to bottobBtn

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+// form validation
 (function () {
   'use strict';
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
   var forms = document.querySelectorAll('.needs-validation');
-
-  // Loop over them and prevent submission
   Array.prototype.slice.call(forms).forEach(function (form) {
     form.addEventListener(
       'submit',
@@ -22,9 +18,30 @@
   });
 })();
 
+// tooltips
 var tooltipTriggerList = [].slice.call(
   document.querySelectorAll('[data-bs-toggle="tooltip"]')
 );
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+// mobile bottomButton scroll event
+const mobileBottomBtn = document.querySelector('#mobileBottomBtn');
+const donationForm = document.querySelector('#donation-form');
+window.addEventListener('scroll', () => {
+  // 當滑動至donationForm，按鈕消失
+  if (donationForm.getBoundingClientRect().top < 200) {
+    mobileBottomBtn.style.display = 'none';
+  } else {
+    mobileBottomBtn.style.display = 'block';
+  }
+  // let scrollY = window.scrollY;
+  // let bodyHight = document.body.offsetHeight;
+  // // 當滑至網頁最底部時，按鈕消失
+  // if (bodyHight - scrollY <= window.innerHeight) {
+  //   mobileBottomBtn.style.display = 'none';
+  // } else {
+  //   mobileBottomBtn.style.display = 'block';
+  // }
 });
